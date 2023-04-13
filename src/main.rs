446d27d240fb0e6001f817865a4cdc6a44eff1a1
Chu-4hun <tapok134224@mail.ru>
2023-04-13 21:07:57 +0300

    refactor <main.rs>: now you just need to pass 2 IDs from id to id like this {rust_parser 0 100}

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,17 @@ pub mod parser;
 pub mod parsers;
 pub mod repo;
 
-use std::{error::Error, io, str::FromStr};
+use std::{error::Error, env};
 
-fn read_values<T: FromStr>() -> Result<Vec<T>, T::Err> {
-    println!("enter releases IDs with whitespaces");
-    let mut s = String::new();
-    io::stdin()
-        .read_line(&mut s)
-        .expect("could not read from stdin");
-    s.trim()
-        .split_whitespace()
-        .map(|word| word.parse())
-        .collect()
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let count_from:u32 =  args[1].parse::<u32>().unwrap_or(0);
+    let count_to:u32 =  args[2].parse::<u32>().unwrap_or(0);
+    dbg!(args);
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    let numbers: Vec<i32> = read_values::<i32>().unwrap_or(vec![3050]);
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -34,8 +25,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .await
         .expect("Error building a connection pool");
 
-    for num in 0..numbers[0] {
-        match parse_anixart(num, &pool).await {
+    for num in count_from..count_to {
+        match parse_anixart(num.try_into().unwrap_or(0), &pool).await {
             Ok(count) => println!("{} new episodes", count),
             Err(e) => {
                 println!("An error: {}; skipped.", e);
